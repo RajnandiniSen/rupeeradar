00eaafaeac19b11ae67adae835993bcdf4889a60
Rajnandini Sen <sen17@illinois.edu>
2026-06-10 19:10:46 -0500

    day 1: add database connection setup

---
+++ backend/app/core/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_engine("postgresql://rupeeradar:rupeeradar123@localhost:5432/rupeeradar")
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+class Base(DeclarativeBase):
+    pass
